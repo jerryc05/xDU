fc12ce19aa401978883c2615db4d5463d28f96ca
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-04-01 21:52:54 -0400

    v0.1.1; single thread version is working

--- src/convert_unit.rs
+++ src/convert_unit.rs
@@ -1,11 +1,52 @@
-const UNIT_TUPLE: [(u64, &str); 6] = [
-  (1024_u64.pow(1), "KiB"),
-  (1024_u64.pow(2), "MiB"),
-  (1024_u64.pow(3), "GiB"),
-  (1024_u64.pow(4), "TiB"),
-  (1024_u64.pow(5), "PiB"),
-  (1024_u64.pow(6), "EiB")
-];
+const UNIT_TUPLE: [(u64, &str); 6] = {
+  let mut i = 0_u32;
+
+  let kib = {
+    1024_u64.pow({
+      i += 1;
+      i
+    })
+  };
+  let mib = {
+    1024_u64.pow({
+      i += 1;
+      i
+    })
+  };
+  let gib = {
+    1024_u64.pow({
+      i += 1;
+      i
+    })
+  };
+  let tib = {
+    1024_u64.pow({
+      i += 1;
+      i
+    })
+  };
+  let pib = {
+    1024_u64.pow({
+      i += 1;
+      i
+    })
+  };
+  let eib = {
+    1024_u64.pow({
+      i += 1;
+      i
+    })
+  };
+
+  [
+    (kib, "KiB"),
+    (mib, "MiB"),
+    (gib, "GiB"),
+    (tib, "TiB"),
+    (pib, "PiB"),
+    (eib, "EiB")
+  ]
+};
 
 pub(crate) fn convert_unit(bytes: u64) -> (f32, &'static str) {
   for (amount, label) in UNIT_TUPLE.iter().rev() {
@@ -13,5 +54,5 @@ pub(crate) fn convert_unit(bytes: u64) -> (f32, &'static str) {
       return (bytes as f32 / *amount as f32, label);
     }
   }
-  (bytes as f32, "  B")
+  (bytes as f32, "B")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![feature(exact_size_is_empty)]
 #![feature(const_int_pow)]
+#![feature(with_options)]
 
 use crate::parse_args::parse_args;
 use crate::convert_unit::convert_unit;
-
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
 mod parse_args;
 mod xdu;
 mod convert_unit;
+
+const FILE_NAME: &str = "log.log";
 
 fn main() {
   let mut config = parse_args();
@@ -18,23 +23,45 @@ fn main() {
         path.symlink_metadata().unwrap().len(),
         path.into_os_string()
       ))
-    } else if let Ok(dir_read) = path.read_dir() {
-      for entry in dir_read {
-        let path_ = entry.unwrap().path();
-        if path_.is_dir() {
-          config.paths.push(path_);
-        } else if let Ok(meta) = path_.symlink_metadata() {
-          result.push((meta.len(), path_.into_os_string()))
+    } else {
+      match path.read_dir() {
+        Ok(dir_read) => {
+          for entry in dir_read {
+            let path_ = entry.unwrap().path();
+            if path_.is_dir() {
+              config.paths.push(path_);
+            } else if let Ok(meta) = path_.symlink_metadata() {
+              result.push((meta.len(), path_.into_os_string()))
+            }
+          }
+        }
+        Err(e) => {
+          eprintln!("Failed to read [{:?}] (aka [{:?}]): {}",
+                    path,
+                    path.canonicalize().unwrap(),
+                    e);
         }
       }
     }
   }
 
-  result.sort_unstable_by(
-    |(a, _), (b, _)| a.cmp(b));
+  result.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
+
+  let file = File::create(FILE_NAME).unwrap();
+  let mut log = BufWriter::new(file);
 
   for (len, path) in &result {
     let (amount, label) = convert_unit(*len);
-    println!("{:>7.3} {}: {}", amount, label, path.to_string_lossy());
+
+    let precision = 3;
+
+
+    write!(log, "{:>1$.2$}", amount, precision + 4, precision)
+      .unwrap_or_else(|_| {});
+    writeln!(log, " {:>3}: {}", label, path.to_string_lossy())
+      .unwrap_or_else(|_| {});
   }
+
+  println!("logs saved to [{}]!",
+           Path::new(FILE_NAME).canonicalize().unwrap().to_string_lossy());
 }
